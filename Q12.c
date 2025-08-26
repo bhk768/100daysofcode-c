60454f98a315eeb9ce9f981413a92b9cc2923ad6
Abhay Rawat <74301608+bhk768@users.noreply.github.com>
2025-08-26 13:33:58 +0530

    Add files via upload

---
+++ Q12.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+int a;
+printf("enter an integer");
+scanf("%d",&a);
+if(a<0)
+printf("Negative");
+else if (a==0)
+printf("zero");
+else
+printf("postive");
+
+
+
+
+
+
+
+    return 0;
+}
