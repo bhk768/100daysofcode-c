25a9f4fc5bb6b5d24a4988a45a1e7fc96a48f0d4
Abhay Rawat <74301608+bhk768@users.noreply.github.com>
2025-10-23 22:51:04 +0530

    Add files via upload

---
+++ Q114.c
@@ -0,0 +1,44 @@
+/*Write a program to take a string s as input. The task is to find the length of the longest substring without repeating characters. Print the length as output.
+Input 1:
+s = "abcabcbb"
+Output 1:
+3
+Explanation 1:
+The answer is "abc", with the length of 3. Note that "bca" and "cab" are also correct answers.*/
+#include <stdio.h>
+#include <string.h>
+
+int lengthOfLongestSubstring(char *s) {
+    int n = strlen(s);
+    int lastIndex[256];
+    int i;
+    for (i = 0; i < 256; i++)
+        lastIndex[i] = -1;
+
+    int maxLen = 0;
+    int start = 0;
+
+    for (i = 0; i < n; i++) {
+        if (lastIndex[(unsigned char)s[i]] >= start)
+            start = lastIndex[(unsigned char)s[i]] + 1;
+
+        lastIndex[(unsigned char)s[i]] = i;
+
+        int windowLen = i - start + 1;
+        if (windowLen > maxLen)
+            maxLen = windowLen;
+    }
+
+    return maxLen;
+}
+
+int main() {
+    char s[100];
+    printf("Enter a string: ");
+    scanf("%s", s);
+
+    int result = lengthOfLongestSubstring(s);
+    printf("Length of the longest substring without repeating characters: %d\n", result);
+
+    return 0;
+}
