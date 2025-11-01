c24ea1918905faa3d75c5f4d7456437dbf9e567d
Abhay Rawat <74301608+bhk768@users.noreply.github.com>
2025-11-01 21:20:30 +0530

    Add files via upload

---
+++ Q120.c
@@ -0,0 +1,35 @@
+/*Write a program to take a string input. Change it to sentence case.
+Input 1:
+str = I am trying to build logic.
+Output 1:
+I Am Trying To Build Logic
+Input 2:
+str = The classes are supposed to start early.
+Output 2:
+The Classes Are Supposed To Start Early.*/
+#include <stdio.h>
+#include <ctype.h>
+int main(){
+    int i;
+char str[100];
+printf("enter the string");
+fgets(str,sizeof(str),stdin);
+for(i=0;str[i]!='\0';i++){
+if(i==0 || str[i-1]==' ')
+str[i]=toupper(str[i]);
+else
+str[i]=tolower(str[i]);
+}
+
+printf("the string after sentence case is %s",str);
+
+
+
+
+
+
+
+
+
+    return 0;
+}
