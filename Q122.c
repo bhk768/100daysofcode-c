5d60a9bb5cb637d0604d1f525d551536589d5747
Abhay Rawat <74301608+bhk768@users.noreply.github.com>
2025-11-11 18:51:17 +0530

    Add files via upload

---
+++ Q122.c
@@ -0,0 +1,31 @@
+/*Write a C program that opens an existing file (e.g., info.txt) and reads its contents using fgets(). The program should print all the lines to the console until EOF (end of file) is reached.
+Input 1:
+File: info.txt (Content: Name: Rahul\nAge: 23)
+Output 1:
+Name: Rahul
+Age: 23
+Explanation 1:
+The program reads each line from info.txt using fgets() and prints it until EOF.*/
+#include <stdio.h>
+int main(){
+FILE *fp;
+char line[200];
+fp=fopen("info.txt","r");
+if(fp==NULL){
+    printf("error could not open the file");
+    return 1;
+}
+while(fgets(line,sizeof(line),fp)!=NULL){
+    printf("%s",line);
+}
+
+fclose(fp);
+
+
+
+
+
+
+
+    return 0;
+}
