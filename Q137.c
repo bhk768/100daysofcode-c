f1c868c53edd520368b032c8e301269641a2a35f
Abhay Rawat <74301608+bhk768@users.noreply.github.com>
2025-11-15 12:38:19 +0530

    Add files via upload

---
+++ Q137.c
@@ -0,0 +1,51 @@
+/*Create an enum for user roles (ADMIN, USER, GUEST) and display messages based on role.
+Input 1:
+GUEST
+Output 1:
+Welcome Guest!
+Explanation 1:
+Enum value determines which message is displayed.*/
+#include <stdio.h>
+#include <string.h>
+enum user{
+admin,user,guest
+};
+int main(){
+char s[100];
+printf("enter admin/user/guest");
+scanf("%s",s);
+enum user roles;
+if(strcmp(s,"admin")==0)
+roles=admin;
+else if(strcmp(s,"user")==0)
+roles=user;
+else if(strcmp(s,"guest")==0)
+roles=guest;
+else{
+printf("wrong input");
+return 0;
+}
+switch(roles){
+    case admin:
+    printf("welcome admin");
+    break;
+    case user:
+    printf("welcome user");
+    break;
+    case guest:
+    printf("welcome guest");
+    break;
+    default:
+    printf("wrong input");
+}
+
+
+
+
+
+
+
+
+
+    return 0;
+}
